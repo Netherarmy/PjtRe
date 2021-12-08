ef1d12ce23eb8f67ecf2624123c94f8f6221a6d4
Netherarmy <92981218+Netherarmy@users.noreply.github.com>
2021-12-08 11:10:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import cv2
 import time
+from PIL import ImageGrab
+import numpy as np
 
 
 longueur = 25
 interval = 0.1
 pix_depart = 0
-t_depart = time.time()
 
 
-def motionDetector(cam, blur, thresh0, thresh1, area, T):
+def motionDetectorcam(cam, blur, thresh0, thresh1, area, T):
+    t = time.time()
     cv2.namedWindow('Frames')
     cv2.namedWindow('Thresh')
     cv2.namedWindow('Security Feed')
@@ -47,10 +49,57 @@ def motionDetector(cam, blur, thresh0, thresh1, area, T):
                         if cv2.waitKey(T) & 0xFF == 27:
                             loop = False
                         t_2 = time.time()
-                        global t_depart
                         global pix_depart
-                        if t_2 - t_depart > interval:
-                            t_depart = time.time()
+                        if t_2 - t > interval:
+                            t = time.time()
+                        v = (a - pix_depart) / interval
+                        print('v = ', v, 'cm/s')
+                        pix_depart = a
+
+
+def motionDetector(blur, thresh0, thresh1, area, T):
+    t = time.time()
+    cv2.namedWindow('Frames')
+    cv2.namedWindow('Thresh')
+    cv2.namedWindow('Security Feed')
+
+    prevFrame = None
+    loop = True
+    while loop:
+        frame = np.array(ImageGrab.grab())
+        gray = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), (blur, blur), 0)
+        if prevFrame is None:
+            prevFrame = gray
+        else:
+            # Ecart entre les frames
+            delta = cv2.absdiff(prevFrame, gray)
+            zeros = np.zeros(gray.shape)
+            if delta != zeros:
+                # Seuil
+                thresh = cv2.threshold(delta, thresh0, thresh1, cv2.THRESH_BINARY)[1]
+                # Dilatation des zones
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                # Contours des zones
+                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                # Affichage des contours
+                for c in contours:
+                    if cv2.contourArea(c) > area:
+                        # Un rectangle incluant la zone
+                        (x, y, w, h) = cv2.boundingRect(c)
+                        # print((w-x)/2)
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+                        a = w * longueur / 1080
+
+                        cv2.imshow("Frames", delta)
+                        cv2.imshow("Thresh", thresh)
+                        cv2.imshow("Security Feed", frame)
+                        # Echap pour quitter dans une fenêtre
+                        if cv2.waitKey(T) & 0xFF == 27:
+                            loop = False
+                        t_2 = time.time()
+                        global pix_depart
+                        if t_2 - t > interval:
+                            t = time.time()
                         v = (a - pix_depart) / interval
                         print('v = ', v, 'cm/s')
                         pix_depart = a
@@ -68,4 +117,4 @@ if __name__ == '__main__':
     AREA = 5000
     # Attente en ms entre 2 capture
     TEMPO = 30
-    motionDetector(CAM, FLOU, SEUIL_0, SEUIL_1, AREA, TEMPO)
+    motionDetector(FLOU, SEUIL_0, SEUIL_1, AREA, TEMPO)
